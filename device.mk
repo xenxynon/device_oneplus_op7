7844256335c9a35eb352a1e60796143d24743051
Pranav Vashi <neobuddy89@gmail.com>
2023-05-15 08:56:52 +0300

    op7: Move msm_irqbalance config to DT
    Change-Id: I0d26e6a0c712ecaeeca3a7435264623ab1080b85

--- device.mk
+++ device.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
